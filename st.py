9b094e98844483fd881402ab44860bee69da7502
natorjunior <natorjuniorcc@gmail.com>
2023-12-20 16:21:45 -0300

    update da visualizacao

--- st.py
+++ st.py
@@ -2,22 +2,29 @@ import streamlit as st
 import os
 st.set_page_config(layout='wide')
 
-st.title('DENTAL')
-col1, col2 = st.columns(2)
+st.title('Endodontia (projeto)')
 
-with col1:
-    list_img = os.listdir('dados/endo')
-    select = st.selectbox('Selecione uma imagem:', list_img)
-    if select:
-        st.header("endo")
-        img_path = os.path.join('dados/endo', select)
-        st.image(img_path, caption=select, use_column_width=True)
+side_sel = st.sidebar.selectbox('Selecione a página:',['ENDO/OUTROS','DENTEX'])
 
-# A coluna col2 está vazia neste exemplo
-with col2:
-    list_img = os.listdir('dados/outros')
-    select = st.selectbox('Selecione (Outros):', list_img)
-    if select:
-        st.header("Outros")
-        img_path = os.path.join('dados/outros', select)
-        st.image(img_path, caption=select, use_column_width=True)
+if side_sel == 'ENDO/OUTROS':
+    col1, col2 = st.columns(2)
+
+    with col1:
+        list_img = os.listdir('dados/endo')
+        select = st.selectbox('Selecione (Endo):', list_img)
+        if select:
+            st.header("endo")
+            radio_end_marc = st.sidebar.radio('Endodontia',['Normal','Marcacao (IA)'],horizontal=False)
+            if radio_end_marc =='Normal':
+                img_path = os.path.join('dados/endo', select)
+                st.image(img_path, caption=select, use_column_width=True)
+
+    with col2:
+        list_img = os.listdir('dados/outros')
+        select = st.selectbox('Selecione (Outros):', list_img)
+        if select:
+            st.header("Outros")
+            img_path = os.path.join('dados/outros', select)
+            st.image(img_path, caption=select, use_column_width=True)
+elif side_sel == 'DENTEX':
+    st.title('PARCIAL DENTEX')
