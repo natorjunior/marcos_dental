f3bf7cfcdded3ec2d77a7ce2b35c9c25f1f98349
natorjunior <natorjuniorcc@gmail.com>
2023-12-20 16:12:27 -0300

    update da visualizacao

--- st.py
+++ st.py
@@ -1,20 +1,23 @@
 import streamlit as st
 import os
+st.set_page_config(layout='wide')
 
 st.title('DENTAL')
 col1, col2 = st.columns(2)
 
-# Lista de imagens na pasta 'dados/endo'
-list_img = os.listdir('dados/endo')
-
-# Seletor para escolher uma imagem da lista
-select = st.selectbox('Selecione uma imagem:', list_img)
-
 with col1:
-    st.header("Imagem Selecionada")
-    img_path = os.path.join('dados/endo', select)
-    st.image(img_path, caption=select, use_column_width=True)
+    list_img = os.listdir('dados/endo')
+    select = st.selectbox('Selecione uma imagem:', list_img)
+    if select:
+        st.header("endo")
+        img_path = os.path.join('dados/endo', select)
+        st.image(img_path, caption=select, use_column_width=True)
 
 # A coluna col2 está vazia neste exemplo
 with col2:
-    pass
+    list_img = os.listdir('dados/outros')
+    select = st.selectbox('Selecione (Outros):', list_img)
+    if select:
+        st.header("Outros")
+        img_path = os.path.join('dados/outros', select)
+        st.image(img_path, caption=select, use_column_width=True)
