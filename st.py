c816a3d00357525feca3b9f48cbc2d63a54cc81f
natorjunior <natorjuniorcc@gmail.com>
2023-12-20 16:54:23 -0300

    :alien:

--- st.py
+++ st.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.applications.vgg19 import preprocess_input
+from tensorflow.keras.preprocessing import image
 import os
+import numpy as np
+
 st.set_page_config(layout='wide')
 
 st.title('Endodontia (projeto)')
@@ -17,7 +22,19 @@ if side_sel == 'ENDO/OUTROS':
             radio_end_marc = st.sidebar.radio('Endodontia',['Normal','Marcacao (IA)'],horizontal=False)
             if radio_end_marc =='Normal':
                 img_path = os.path.join('dados/endo', select)
+                loaded_model = tf.keras.models.load_model('test1_vgg19_0_75.h5')
+                img = image.load_img(img_path, target_size=(512, 512))
+                img_array = image.img_to_array(img)
+                img_array = np.expand_dims(img_array, axis=0)
+                img_array = preprocess_input(img_array)
+                pred = loaded_model.predict(img_array)
+                #st.write(pred)
+                if not np.argmax(pred):
+                    st.success('endo (previsao da IA)', icon="✅")
+                else:
+                    st.error('outros (previsao da IA)', icon="🚨")
                 st.image(img_path, caption=select, use_column_width=True)
+                
 
     with col2:
         list_img = os.listdir('dados/outros')
@@ -25,6 +42,17 @@ if side_sel == 'ENDO/OUTROS':
         if select:
             st.header("Outros")
             img_path = os.path.join('dados/outros', select)
+            loaded_model = tf.keras.models.load_model('test1_vgg19_0_75.h5')
+            img = image.load_img(img_path, target_size=(512, 512))
+            img_array = image.img_to_array(img)
+            img_array = np.expand_dims(img_array, axis=0)
+            img_array = preprocess_input(img_array)
+            pred = loaded_model.predict(img_array)
+            #st.write(pred)
+            if not np.argmax(pred):
+                st.success('endo (previsao da IA)', icon="✅")
+            else:
+                st.error('outros (previsao da IA)', icon="🚨")
             st.image(img_path, caption=select, use_column_width=True)
 elif side_sel == 'DENTEX':
     st.title('PARCIAL DENTEX')
